5a4a95c405d765fa7abc78d67be147d57f6ce06c
Mohsen-Ghazagh <mohsenmirzaalighazagh@gmail.com>
2025-04-05 17:58:58 +0330

    404comit

---
+++ 404.php
@@ -0,0 +1,15 @@
+﻿ <?php 
+get_header(); 
+?>
+		<div class="box_head_archive">
+			<h1>صفحه ای یافت نشد !</h1>
+			<div class="box_desc_head_archive">
+				404
+			</div>
+		</div>
+	
+	</div>
+	
+</div>
+
+<?php get_footer(); ?>
